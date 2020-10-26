f03e3eb1f0dba4214bffe01362e5c4bd1c9bea72
trinhtan <lop12a1trinhvantan@gmail.com>
2020-10-26 16:08:57 +0700

    Multi token for PreSale

--- scripts/masterFactorySetup.js
+++ scripts/masterFactorySetup.js
@@ -1,12 +1,20 @@
 const {
   transferIPhoneTokenOwnership,
   mintPhoneTokenForPreSale,
+  Tokens,
+  addTokenToPreSale,
 } = require('./masterFactoryScripts');
 
 module.exports = async () => {
   try {
     await transferIPhoneTokenOwnership();
     await mintPhoneTokenForPreSale();
+
+    // DAI
+    await addTokenToPreSale(Tokens[0].address);
+
+    // LINK
+    await addTokenToPreSale(Tokens[2].address);
     process.exit();
   } catch (err) {
     console.log(err);
